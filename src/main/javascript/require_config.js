f5ac5fa3b1358159ae4336c1201009c2f1f217a4
Paul Roberts <proberts@cj.com>
2013-02-10 12:11:03 -0800

    Migrating the first test over to use sinon for mocking the http request

--- src/main/javascript/require_config.js
+++ src/main/javascript/require_config.js
@@ -9,13 +9,16 @@ var require = {
                 "jquery"
             ],
             exports: "Backbone"
+        },
+        sinon: {
+            exports: "sinon"
         }
     },
     paths: {
         jquery: "http://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min",
         underscore: "http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min",
         backbone: "http://cdnjs.cloudflare.com/ajax/libs/backbone.js/0.9.10/backbone-min",
-        //backbone: "http://backbonejs.org/backbone",
+        sinon: "lib/sinon-1.5.2",
         text: "http://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.3/text"
     }
 };
--- src/test/javascript/todo_test.qunit.js
+++ src/test/javascript/todo_test.qunit.js
@@ -1,27 +1,37 @@
-define(["models/todo"], function (Todo) {
+define([
+    "models/todo",
+    "sinon"
+], function (Todo, sinon) {
     module("Todo Model Test", {
         setup: function () {
-            var that = this;
-            that.saveCalled = {};
-            that.savedTodo = {};
-
-            $.ajax = function (opts) {
-                that.saveCalled = opts;
-                that.savedTodo = $.parseJSON(opts.data);
-            };
+            this.server = sinon.fakeServer.create();
         },
         teardown: function () {
+            this.server.restore();
         }
     });
 
     test("This should pass", function () {
-        var toodoo = new Todo();
+        var server = this.server,
+            requestedTodo,
+            toodoo;
+
+        this.server.respondWith("POST", "/todo",
+            [200, { "Content-Type": "application/json" }, '{ "id": 1, "title": "", "completed": false}']);
+
+        toodoo = new Todo();
         equal(toodoo.get("completed"), false);
-        deepEqual(this.saveCalled, {});
+        equal(server.requests.length, 0);
 
         toodoo.toggle();
         equal(toodoo.get("completed"), true);
-        equal(this.saveCalled.dataType, "json");
-        equal(this.savedTodo.completed, true);
+        equal(server.requests.length, 1);
+
+        equal(server.requests[0].async, true);
+        equal(server.requests[0].requestHeaders["Content-Type"], "application/json;charset=utf-8");
+
+        requestedTodo = $.parseJSON(server.requests[0].requestBody);
+        equal(requestedTodo.title, "");
+        equal(requestedTodo.completed, true);
     });
 });
